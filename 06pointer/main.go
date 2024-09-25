190eb06b68b0ac0b200112c318cfc58e00b13941
pratikdev <pratikdevofficial1@gmail.com>
2024-09-26 03:42:14 +0600

    pointers

--- 06pointer/main.go
+++ 06pointer/main.go
@@ -5,4 +5,9 @@ import "fmt"
 func main() {
 	var pointer *int
 	fmt.Println("Value of pointer is:", pointer)
+
+	myName := "pratik dev"
+	var myNamePointer = &myName
+	fmt.Println("My name pointer is:", myNamePointer)
+	fmt.Println("My name pointer value is:", *myNamePointer)
 }
---
+++ README.md
@@ -0,0 +1,7 @@
+# Trying to learn Go
+
+This repo have a bash script that helps to initialize a go module and create a main.go file with the given name
+
+```bash
+./go.init.sh 01hello
+```
